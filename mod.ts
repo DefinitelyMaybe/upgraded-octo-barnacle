752e8553d5356cde752022605c271a5fd51ae37c
John Doe <DefinitelyMaybe@users.noreply.github.com>
2021-03-30 10:28:23 +1300

    new response

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 console.log("hello world");
 
 addEventListener("fetch", (event) => {
-    const response = new Response("Hello World!", {
+    const response = new Response("Hello Worldssssssss!", {
       headers: { "content-type": "text/plain" },
     });
     event.respondWith(response);
